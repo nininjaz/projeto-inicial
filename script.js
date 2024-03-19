944849ac837b70a7f5580659995e7970ba64e137
nininjaz <lannycarolinyalves@gmail.com>
2024-03-19 08:22:48 -0300

    formulario

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
 function alertar(event){
-    alert("Você clicou no botão!!" + nome.value);
+    //alert("Você clicou no botão!!" + nome.value);
+
+    saida.innerText = "Nome: " + nome.value+
+    "\n Email: " + email.value+
+    "\n Telefone: " + telefone.value+
+    "\n CEP: " + cep.value+
+    "\n Rua: " + rua.value+
+    "\n Numero: " + numero.value+
+    "\n Complemento: " + complemento.value+
+    "\n Bairro: " + bairro.value+
+    "\n Cidade: " + cidade.value+
+    "\n Estado: " + estado.value;
+
+
 }
